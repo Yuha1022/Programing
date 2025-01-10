ccc08cae7125b7a4b09875bc82521e94368b9a0b
(・ー・) <yuha-kut@noMacBook-Pro.local>
2025-01-10 22:20:06 +0900

    広告管理画面完成

--- React/src/App.js
+++ React/src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdmiLoginPage from './admi_login/admi_login'; // ログインページ
 import AdmiTopPage from './admi_top/admi_top'; // トップページ
-import AdManagementPage from './admi_advertisement/admi_advertisement'; // 広告管理画面
+import AdmiAdvertisementPage from './admi_advertisement/admi_advertisement'; // 広告管理画面
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-        <Route path="/" element={<AdManagementPage/>} /> {/* 初期表示でログインページ */}
+        <Route path="/" element={<AdmiAdvertisementPage/>} /> {/* 初期表示でログインページ */}
           <Route path="/admi_login" element={<AdmiLoginPage/>} /> {/* ログインページ */}
           <Route path="/admi_top" element={<AdmiTopPage/>} /> {/* ログインページ */}
-          <Route path="/admi_top" element={<AdManagementPage/>} /> {/* 広告管理画面 */}
+          <Route path="/admi_advertisement" element={<AdmiAdvertisementPage/>} /> {/* 広告管理画面 */}
         </Routes>
       </div>
     </Router>
